7886f34c4bc9b42b728ad5abc3774bbca35b77c4
Mahmudur <mahmudhera93@gmail.com>
2023-11-12 13:11:44 -0500

    added dir name

--- tests/test_standardize_output.py
+++ tests/test_standardize_output.py
@@ -9,10 +9,10 @@ class TestScript:
         script_full_path = os.path.join(script_dir, 'standardize_yacht_output.py')
         assert os.path.exists(script_full_path)
 
-        yacht_output = os.path.join(os.path.dirname(__file__), 'testdata/result.xlsx')
+        yacht_output = os.path.join(os.path.dirname(__file__), 'testdata/standardize_output_testdata/result.xlsx')
         assert os.path.exists(yacht_output)
 
-        genome_to_taxid = os.path.join(os.path.dirname(__file__), 'testdata/toy_genome_to_taxid.tsv')
+        genome_to_taxid = os.path.join(os.path.dirname(__file__), 'testdata/standardize_output_testdata/toy_genome_to_taxid.tsv')
         assert os.path.exists(genome_to_taxid)
 
         outdir = os.path.join(os.path.dirname(__file__), 'testdata')
