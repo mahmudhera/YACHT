de980458b1342651412e8001261f16411d36adec
Mahmudur <mahmudhera93@gmail.com>
2023-11-12 13:15:52 -0500

    added no yacht test

--- tests/test_standardize_output.py
+++ tests/test_standardize_output.py
@@ -2,7 +2,7 @@ import unittest
 import os
 import subprocess
 
-class TestScript:
+class TestScript(unittest.TestCase):
     def test_everything_exists(self):
         script_dir = os.path.dirname(os.path.dirname(os.path.realpath(__file__)))
         script_dir = os.path.join(script_dir, 'srcs')
@@ -22,6 +22,29 @@ class TestScript:
         res = subprocess.run(cmd, shell=True, check=True)
         assert res.returncode == 0
         assert os.path.exists(os.path.join(outdir, 'cami_result.cami'))
+
+    def test_no_yacht_output(self):
+        script_dir = os.path.dirname(os.path.dirname(os.path.realpath(__file__)))
+        script_dir = os.path.join(script_dir, 'srcs')
+        script_full_path = os.path.join(script_dir, 'standardize_yacht_output.py')
+        assert os.path.exists(script_full_path)
+
+        yacht_output = os.path.join(os.path.dirname(__file__), 'testdata/standardize_output_testdata/result_nonexisting.xlsx')
+        assert os.path.exists(yacht_output)
+
+        genome_to_taxid = os.path.join(os.path.dirname(__file__), 'testdata/standardize_output_testdata/toy_genome_to_taxid.tsv')
+        assert os.path.exists(genome_to_taxid)
+
+        outdir = os.path.join(os.path.dirname(__file__), 'testdata')
+        assert os.path.exists(outdir)
+
+        cmd = f"python {script_full_path} --sheet_name min_coverage0.2 --genome_to_taxid {genome_to_taxid} --outfile_prefix cami_result --outdir {outdir}"
+        res = subprocess.run(cmd, shell=True, check=False)
+        assert res.returncode == 1
+
+        cmd = f"python {script_full_path} --yacht_output {yacht_output} --sheet_name min_coverage0.2 --genome_to_taxid {genome_to_taxid} --outfile_prefix cami_result --outdir {outdir}"
+        res = subprocess.run(cmd, shell=True, check=True)
+        assert res.returncode == 0
 
 
 if __name__ == '__main__':
